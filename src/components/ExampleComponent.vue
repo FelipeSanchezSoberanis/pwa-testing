8dc9f76cd3ec637c27b5b4fea51ca51a4833cd27
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-07-19 08:43:12 -0500

    Formatting pass

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -7,26 +7,19 @@
       </li>
     </ul>
     <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
+    <p>Active: {{ active ? "yes" : "no" }}</p>
     <p>Clicks on todos: {{ clickCount }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  PropType,
-  computed,
-  ref,
-  toRef,
-  Ref,
-} from 'vue';
-import { Todo, Meta } from './models';
+import { defineComponent, PropType, computed, ref, toRef, Ref } from "vue";
+import { Todo, Meta } from "./models";
 
 function useClickCount() {
   const clickCount = ref(0);
   function increment() {
-    clickCount.value += 1
+    clickCount.value += 1;
     return clickCount.value;
   }
 
@@ -39,7 +32,7 @@ function useDisplayTodo(todos: Ref<Todo[]>) {
 }
 
 export default defineComponent({
-  name: 'ExampleComponent',
+  name: "ExampleComponent",
   props: {
     title: {
       type: String,
@@ -57,8 +50,8 @@ export default defineComponent({
       type: Boolean
     }
   },
-  setup (props) {
-    return { ...useClickCount(), ...useDisplayTodo(toRef(props, 'todos')) };
-  },
+  setup(props) {
+    return { ...useClickCount(), ...useDisplayTodo(toRef(props, "todos")) };
+  }
 });
 </script>
