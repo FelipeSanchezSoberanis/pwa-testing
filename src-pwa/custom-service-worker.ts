65ce1d7f93afdf618b6ad55355f7a242a3b5bfef
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-07-19 09:07:35 -0500

    PWA

--- src-pwa/custom-service-worker.ts
+++ src-pwa/custom-service-worker.ts
@@ -6,13 +6,13 @@
 
 declare const self: ServiceWorkerGlobalScope & typeof globalThis;
 
-import { clientsClaim } from 'workbox-core';
+import { clientsClaim } from "workbox-core";
 import {
-  precacheAndRoute,
-  cleanupOutdatedCaches,
-  createHandlerBoundToURL,
-} from 'workbox-precaching';
-import { registerRoute, NavigationRoute } from 'workbox-routing';
+    precacheAndRoute,
+    cleanupOutdatedCaches,
+    createHandlerBoundToURL
+} from "workbox-precaching";
+import { registerRoute, NavigationRoute } from "workbox-routing";
 
 self.skipWaiting();
 clientsClaim();
@@ -24,11 +24,11 @@ cleanupOutdatedCaches();
 
 // Non-SSR fallback to index.html
 // Production SSR fallback to offline.html (except for dev)
-if (process.env.MODE !== 'ssr' || process.env.PROD) {
-  registerRoute(
-    new NavigationRoute(
-      createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML),
-      { denylist: [/sw\.js$/, /workbox-(.)*\.js$/] }
-    )
-  );
+if (process.env.MODE !== "ssr" || process.env.PROD) {
+    registerRoute(
+        new NavigationRoute(
+            createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML),
+            { denylist: [/sw\.js$/, /workbox-(.)*\.js$/] }
+        )
+    );
 }
