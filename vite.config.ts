fcbeb80db4b82ccaac6f9e66fe99b11cf3092200
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-07-19 13:36:37 -0500

    Maybe?

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,21 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        short_name: "VueJS",
+        icons: [
+          { src: "src/assets/vue.png", sizes: "192x192", type: "image/png" },
+          { src: "src/assets/vue.png", sizes: "512x512", type: "image/png" },
+        ],
+        start_url: "/",
+        display: "standalone",
+        prefer_related_applications: false,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
