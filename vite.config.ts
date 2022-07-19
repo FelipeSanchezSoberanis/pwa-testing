6722352a94cc1c13d014e679273f3472d350a768
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-07-19 13:22:27 -0500

    Testing again

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      mode: "production",
-      base: "/",
-      srcDir: "src",
-      filename: "sw.js",
-      includeAssets: ["/favicon.png"],
-      strategies: "generateSW",
-    }),
-  ],
+  plugins: [vue(), VitePWA()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
